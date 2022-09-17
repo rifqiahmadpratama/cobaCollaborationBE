647998d1b063f87e9655c62ebeec4c99a87c3442
karomahlailatul <karomahlailatul@gmail.com>
2022-09-17 17:04:33 +0700

    error bug bcrypt

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,5 +1,5 @@
 const { v4: uuidv4 } = require("uuid");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const usersModel = require("../models/users");
 const commonHelper = require("../helper/common");
