96b6ed20b0672647b2c17dbaa59661c04d2ddfd4
karomahlailatul <karomahlailatul@gmail.com>
2022-09-17 18:53:34 +0700

    testing google cloud

---
+++ src/middlewares/googledriveservice.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const { google } = require('googleapis')
+
+
+const authenticateGoogle = () => {
+    const auth = new google.auth.GoogleAuth({
+        keyFile: `${__dirname}/service-account.json`,
+        scopes: "https://www.googleapis.com/auth/drive",
+    });
+    return auth;
+};
+
+
+const uploadToGoogleDrive = async (file, auth) => {
+    const fileMetadata = {
+        name: file.originalname,
+        parents: ["1VpuBbCm_Gjmzok9jE1PNlRPIFPnpLQzO"], // Change it according to your desired parent folder id
+    };
+
+    const media = {
+        mimeType: file.mimetype,
+        body: fs.createReadStream(file.path),
+    };
+
+    const driveService = google.drive({ version: "v3", auth });
+
+    const response = await driveService.files.create({
+        requestBody: fileMetadata,
+        media: media,
+        fields: "id",
+    });
+    return response;
+};
+
+const deleteFile = (filePath) => {
+    fs.unlink(filePath, () => {
+        console.log("file deleted");
+    });
+};
+
+module.exports = {
+    authenticateGoogle,
+    uploadToGoogleDrive,
+    deleteFile
+}
--- src/routes/users.js
+++ src/routes/users.js
@@ -9,6 +9,8 @@ const upload = require("../middlewares/upload");
 //     validateChangeEmail,
 //     validateChangePassword } = require('../middlewares/common')
 
+const { authenticateGoogle, uploadToGoogleDrive, deleteFile } = require("../middlewares/googledriveservice")
+
 router
   .post("/register", ControllerUsers.registerAccount)
   .post("/login", ControllerUsers.loginAccount)
@@ -17,6 +19,25 @@ router
   .put("/profile", protect, upload.single("picture"), ControllerUsers.profileAccount)
   .put("/profile/changeEmail", protect, ControllerUsers.changeEmail)
   .put("/profile/changePassword", protect, ControllerUsers.changePassword)
-  .delete("/profile", protect, ControllerUsers.profileAccount);
+  .delete("/profile", protect, ControllerUsers.profileAccount)
+
+
+  .post("/uploadfiledrive", upload.single("picture"), async (req, res) => {
+    try {
+      if (!req.file) {
+        res.status(400).send("No file uploaded.");
+        return;
+      }
+      const auth = authenticateGoogle();
+      const response = await uploadToGoogleDrive(req.file, auth);
+      deleteFile(req.file.path);
+      res.status(200).json({ response });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  );
+
+
 
 module.exports = router;
