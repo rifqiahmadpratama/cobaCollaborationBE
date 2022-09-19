652593a7a046f08bdc975747a60073538f4654e1
karomahlailatul <karomahlailatul@gmail.com>
2022-09-19 18:32:46 +0700

    set origin cors

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
-app.use(cors());
+
+app.use(cors({
+  origin: '*'
+}));
+
 app.use(helmet());
 app.use(xss());
 
