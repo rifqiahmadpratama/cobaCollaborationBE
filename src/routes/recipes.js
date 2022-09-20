5e0a62dc37581c0ac1a98b492e50789052ec89be
karomahlailatul <karomahlailatul@gmail.com>
2022-09-20 07:19:58 +0700

    fixed testing recipes from user

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -4,12 +4,12 @@ const recipesController = require("../controller/recipes");
 const { protect } = require("../middlewares/auth");
 const upload = require("../middlewares/upload");
 
-// router.get("/", recipesController.getPaginationRecipes);
-// router.get("/:id", recipesController.getRecipes);
+router.get("/", recipesController.getPaginationRecipes);
+router.get("/:id", recipesController.getRecipes);
 
-// router.post("/", protect, upload.single("photo_id"), recipesController.insertRecipes);
-// router.put("/:id", protect, upload.single("photo_id"), recipesController.updateRecipes);
-// router.delete("/:id", protect, recipesController.deleteRecipes);
+router.post("/", protect, upload.single("photo_id"), recipesController.insertRecipes);
+router.put("/:id", protect, upload.single("photo_id"), recipesController.updateRecipes);
+router.delete("/:id", protect, recipesController.deleteRecipes);
 
 
 router.get('/usersrecipes/:id', recipesController.getPaginationRecipestByUser);
